00887dd5f9d538d5362df96e346cb639c14cd158
Lukas <lukas_n2@gmx.de>
2025-11-27 20:02:43 +0100

    connection rights

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // listen on all addresses
+    port: 5173,
+    allowedHosts: ['nowakl.org', 'localhost', '127.0.0.1'],
+  },
 })
