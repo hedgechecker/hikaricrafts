3958914d41a77f9c474c441647161f13b1fb84cd
Lukas Nowak <lukas_n2@gmx.de>
2026-06-20 15:42:03 +0200

    changed chuncking

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from 'rollup-plugin-visualizer';
+
 
 export default defineConfig({
-  plugins: [react()
+  plugins: [react(),
+    
+    visualizer({
+      open: true, // Automatically opens the report in your browser after building
+      filename: 'bundle-analysis.html',
+    }),
   ],
   server: {
     host: "0.0.0.0",      // listen on all network interfaces
@@ -14,18 +21,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 600,
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) return 'vendor-three';     
-            if (id.includes('react-dom') || id.includes('react/')) {
-              return 'vendor-react';
-            }       
-            return 'vendor';
-          }
-        },
-      },
-    },
+    cssCodeSplit: true,
   },
 });
--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import App from "./pages/KumikoEditor/KumikoEditor.tsx";
 import Login from "./pages/Authentication/Login.tsx";
 import "./theme.css";
 import { Register } from "./pages/Authentication/Register.tsx";
@@ -7,9 +6,16 @@ import Feedback from "./pages/Contact/Feedback.tsx";
 import AllProductsPage from "./features/products/AllProducts.tsx";
 import SingleProduct from "./pages/Product/SingleProduct.tsx";
 import Contact from "./pages/Contact/Contact.tsx";
-import WireArtEditor from "./pages/WireArtEditor/WireArtEditor.tsx";
-import Overview from "./features/WireArtEditor/components/Overview.tsx";
 import { useDialog } from "./features/global/useDialog.tsx";
+import { lazy, Suspense } from "react";
+
+const EditorPage = lazy(() => import("./pages/KumikoEditor/KumikoEditor.tsx"));
+const Overview = lazy(
+  () => import("./features/WireArtEditor/components/Overview.tsx"),
+);
+const WireArtEditor = lazy(
+  () => import("./pages/WireArtEditor/WireArtEditor.tsx"),
+);
 
 export default function Routing() {
   const { dialogComponent } = useDialog();
@@ -18,30 +24,38 @@ export default function Routing() {
     <>
       <div id="tooltip-root"></div>
       {dialogComponent}
-      <Routes>
-        <Route path="/" element={<Feedback />} />
-        <Route path="/all" element={<AllProductsPage />} />
-        <Route
-          path="/oboereedsetui"
-          element={<SingleProduct key={1} id={1} />}
-        />
-        <Route path="/fluteCase" element={<SingleProduct key={2} id={2} />} />
-        <Route
-          path="/klarinettreedsetui"
-          element={<SingleProduct key={3} id={3} />}
-        />
-        <Route
-          path="/saxophonreedsetui"
-          element={<SingleProduct key={4} id={4} />}
-        />
-        <Route path="/piccoloCase" element={<SingleProduct key={5} id={5} />} />
-        <Route path="/kontakt" element={<Contact />} />
-        <Route path="/kumiko" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/wireart" element={<Overview />} />
-        <Route path="/wirearteditor" element={<WireArtEditor />} />
-      </Routes>
+
+      <Suspense
+        fallback={<div className="loading-spinner">Loading page...</div>}
+      >
+        <Routes>
+          <Route path="/" element={<Feedback />} />
+          <Route path="/all" element={<AllProductsPage />} />
+          <Route
+            path="/oboereedsetui"
+            element={<SingleProduct key={1} id={1} />}
+          />
+          <Route path="/fluteCase" element={<SingleProduct key={2} id={2} />} />
+          <Route
+            path="/klarinettreedsetui"
+            element={<SingleProduct key={3} id={3} />}
+          />
+          <Route
+            path="/saxophonreedsetui"
+            element={<SingleProduct key={4} id={4} />}
+          />
+          <Route
+            path="/piccoloCase"
+            element={<SingleProduct key={5} id={5} />}
+          />
+          <Route path="/kontakt" element={<Contact />} />
+          <Route path="/kumiko" element={<EditorPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/wireart" element={<Overview />} />
+          <Route path="/wirearteditor" element={<WireArtEditor />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
